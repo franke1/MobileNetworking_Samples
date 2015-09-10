9846e42a0e170677eb3db3c4a91449a2b0cd0862
Lee Jaehoon <wannabewize@gmail.com>
2015-09-10 12:10:24 +0900

    udp echo server

--- Socket/Server/udp_echo_server.js
+++ Socket/Server/udp_echo_server.js
@@ -1,28 +1,28 @@
-var net = require('net');
-var server = net.createServer(function (socket) {
-   console.log('create server ' + socket.remoteAddress);
-   console.log('connect ' + socket.remoteAddress);
+var dgram = require('dgram');
 
-   socket.on('connect', function () {
+var port = 3001;
+var memberships = [];
 
-   });
-   socket.on('data', function (data) {
-      console.log(socket.remoteAddress + ' : ' + data);
-      socket.write(data);
-   });
-   socket.on('end', function () {
-      console.log('connection end')
-   })
-
+var socket = dgram.createSocket('udp4', function(msg, rinfo) {
+   console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port);
+   console.log('Received : ' + msg.toString('utf-8'));
+   // console.log('message : ', msg);
+   
+   console.log('rinfo : ', rinfo);
+	socket.send(msg, 0, msg.length, rinfo.port, rinfo.address);
 });
-
-server.listen(3000);
-
+socket.bind(port);
 
 // 서버 IP 주소 얻기
 var serverIp = require('./serverIp');
+socket.on('listening', function() {
+	console.log('listening @ ' + serverIp.getIPAddress() + ':' + port);
+});
 
-console.log('Echo server is running on ' + serverIp.getIPAddress() + ':3000');
+socket.on('close', function() {
+	console.log('Socket closed');
+});
 
-
-
+socket.on('error', function(err) {
+   console.error('Error', err);
+});
