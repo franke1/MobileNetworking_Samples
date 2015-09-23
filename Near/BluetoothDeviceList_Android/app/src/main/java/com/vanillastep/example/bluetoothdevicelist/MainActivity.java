2dfb767054512ad0885607f0f4e861218ec7b738
Lee Jaehoon <wannabewize@gmail.com>
2015-09-23 16:29:40 +0900

    기기 타입과 서비스도 함께 출력

--- Near/BluetoothDeviceList_Android/app/src/main/java/com/vanillastep/example/bluetoothdevicelist/MainActivity.java
+++ Near/BluetoothDeviceList_Android/app/src/main/java/com/vanillastep/example/bluetoothdevicelist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.vanillastep.example.bluetoothdevicelist;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,11 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +27,12 @@ public class MainActivity extends AppCompatActivity {
    private static final String TAG = "Bluetooth-Sample";
    private static final int BLUETOOTH_ENABLE_REQUEST = 1;
    private BluetoothAdapter bluetoothAdapter;
-   private ListView pairedDeviceList;
-   private ArrayAdapter pairedDeviceAdapter;
-   private ListView newDeviceList;
-   private ArrayAdapter newDeviceAdapter;
+
    private TextView stateLabel;
+
+   private ArrayAdapter<DeviceInfo> newDeviceListAdapter;
+   private ArrayAdapter<DeviceInfo> pairedDeviceListAdapter;
+
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +57,13 @@ public class MainActivity extends AppCompatActivity {
          filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
          registerReceiver(mReceiver, filter);
 
-         pairedDeviceList = (ListView)findViewById(R.id.pairedDeviceList);
-         pairedDeviceAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-         pairedDeviceList.setAdapter(pairedDeviceAdapter);
+         ListView pairedDeviceList = (ListView) findViewById(R.id.pairedDeviceList);
+         pairedDeviceListAdapter = new ArrayAdapter<DeviceInfo>(this, android.R.layout.simple_list_item_1);
+         pairedDeviceList.setAdapter(pairedDeviceListAdapter);
 
-         newDeviceList = (ListView) findViewById(R.id.newDeviceList);
-         newDeviceAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-         newDeviceList.setAdapter(newDeviceAdapter);
+         ListView newDeviceList = (ListView) findViewById(R.id.newDeviceList);
+         newDeviceListAdapter = new ArrayAdapter<DeviceInfo>(this, android.R.layout.simple_list_item_1);
+         newDeviceList.setAdapter(newDeviceListAdapter);
       }
    }
 
@@ -73,18 +79,20 @@ public class MainActivity extends AppCompatActivity {
          stateLabel.setText("블루투스 장치 검색 중지");
       }
       else {
-         newDeviceAdapter.clear();
-         pairedDeviceAdapter.clear();
+         newDeviceListAdapter.clear();
+         pairedDeviceListAdapter.clear();
 
          // 페어링됐던 기기 목록
          Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+         Log.d(TAG, "Paried Device : " + pairedDevices.size());
          if (pairedDevices.size() > 0) {
             // Loop through paired devices
             for (BluetoothDevice device : pairedDevices) {
-               pairedDeviceAdapter.add(device.getName() + "\n" + device.getAddress());
+               DeviceInfo info = DeviceInfo.getInfo(device);
+               pairedDeviceListAdapter.add(info);
             }
          }
-         pairedDeviceAdapter.notifyDataSetChanged();
+         pairedDeviceListAdapter.notifyDataSetChanged();
 
          // 주변 블루투스 기기 검색 시작
          Log.d(TAG, "Start discovery");
@@ -115,11 +123,9 @@ public class MainActivity extends AppCompatActivity {
             String name = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
             BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-            Log.d(TAG, "name : " + name);
-            Log.d(TAG, "Address : " + device.getAddress());
-
-            newDeviceAdapter.add(device.getName() + "\n" + device.getAddress());
-            newDeviceAdapter.notifyDataSetChanged();
+            DeviceInfo info = DeviceInfo.getInfo(device);
+            newDeviceListAdapter.add(info);
+            newDeviceListAdapter.notifyDataSetChanged();
          }
          else if ( BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
             // 블루투스 기기 검색 시작
@@ -141,5 +147,67 @@ public class MainActivity extends AppCompatActivity {
    protected void onStop() {
       super.onStop();
       unregisterReceiver(mReceiver);
+   }
+}
+
+class DeviceInfo {
+   String name;
+   String address;
+   String type;
+   String service;
+
+   @Override
+   public String toString() {
+      return "name : " + name + "\naddress : " + address + "\ntype : " + type + "\nservice : " + service;
+   }
+
+   static DeviceInfo getInfo(BluetoothDevice device) {
+      DeviceInfo info = new DeviceInfo();
+      info.name = device.getName();
+      info.address = device.getAddress();
+
+      BluetoothClass btClass = device.getBluetoothClass();
+
+      String type = null;
+
+      switch (btClass.getMajorDeviceClass()) {
+         case BluetoothClass.Device.Major.AUDIO_VIDEO:
+            type = "AUDIO_VIDEO"; break;
+         case BluetoothClass.Device.Major.COMPUTER:
+            type = "COMPUTER"; break;
+         case BluetoothClass.Device.Major.HEALTH:
+            type = "HEALTH"; break;
+         case BluetoothClass.Device.Major.IMAGING:
+            type = "IMAGING"; break;
+         case BluetoothClass.Device.Major.MISC:
+            type = "MISC"; break;
+         case BluetoothClass.Device.Major.NETWORKING:
+            type = "NETWORKING"; break;
+         case BluetoothClass.Device.Major.PERIPHERAL:
+            type = "PERIPHERAL"; break;
+         case BluetoothClass.Device.Major.PHONE:
+            type = "PHONE"; break;
+         case BluetoothClass.Device.Major.TOY:
+            type = "TOY"; break;
+         case BluetoothClass.Device.Major.UNCATEGORIZED:
+            type = "UNCATEGORIZED"; break;
+         case BluetoothClass.Device.Major.WEARABLE:
+            type = "WEARABLE"; break;
+      }
+
+      info.type = type + " (" + btClass.getDeviceClass() + ")";
+
+      StringBuffer buffer = new StringBuffer();
+      if ( btClass.hasService(BluetoothClass.Service.NETWORKING) ) buffer.append("NETWORKING ");
+      if ( btClass.hasService(BluetoothClass.Service.AUDIO) ) buffer.append("AUDIO ");
+      if ( btClass.hasService(BluetoothClass.Service.CAPTURE) ) buffer.append("CAPTURE ");
+      if ( btClass.hasService(BluetoothClass.Service.INFORMATION)) buffer.append("INFORMATION ");
+      if ( btClass.hasService(BluetoothClass.Service.OBJECT_TRANSFER)) buffer.append("OBJECT_TRANSFER ");
+      if ( btClass.hasService(BluetoothClass.Service.POSITIONING)) buffer.append("POSITIONING ");
+      if ( btClass.hasService(BluetoothClass.Service.RENDER)) buffer.append("RENDER ");
+      if ( btClass.hasService(BluetoothClass.Service.TELEPHONY)) buffer.append("TELEPHONY ");
+      info.service = buffer.toString();
+
+      return info;
    }
 }
