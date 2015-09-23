c358fed136bc04dccb60ff034863d5ed5e48d9a9
Lee Jaehoon <wannabewize@gmail.com>
2015-09-24 00:15:50 +0900

    LE 기기 검색

--- Near/BTLE_Android/app/src/main/java/com/vanillastep/example/btle/MainActivity.java
+++ Near/BTLE_Android/app/src/main/java/com/vanillastep/example/btle/MainActivity.java
@@ -1,29 +1,27 @@
 package com.vanillastep.example.btle;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
-import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,40 +33,57 @@ public class MainActivity extends AppCompatActivity {
 
    private static final String TAG = "BLTE_Sample";
    private BluetoothLeScanner mScanner;
+   private BluetoothAdapter mBluetoothAdapter;
+   private boolean mScanning; // 상태값이 없다.
 
    List<ScanResult> deviceList = new ArrayList<>();
    DeviceAdapter deviceAdapter;
+   private static final long SCAN_PERIOD = 10000;
+   private TextView mStatusView;
+
+   private Handler mHandler;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
-      ListView listView = (ListView) findViewById(R.id.listView);
-      deviceAdapter = new DeviceAdapter();
-      listView.setAdapter(deviceAdapter);
-      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            ScanResult result = (ScanResult) deviceAdapter.getItem(position);
-            BluetoothDevice device = result.getDevice();
-
-            connectDevice(device);
-         }
-      });
+      mHandler = new Handler();
 
       if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
          Toast.makeText(this, "블루투스 LE 지원 안함", Toast.LENGTH_SHORT).show();
          finish();
       }
 
-      BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+      Button discoverButton = (Button) findViewById(R.id.discoverButton);
+      discoverButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            toggleScan();
+         }
+      });
+
+      mStatusView = (TextView) findViewById(R.id.statusView);
+
+      ListView listView = (ListView) findViewById(R.id.listView);
+      deviceAdapter = new DeviceAdapter();
+      listView.setAdapter(deviceAdapter);
+      // 리스트 선택 - 연결
+      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            ScanResult result = (ScanResult) deviceAdapter.getItem(position);
+            BluetoothDevice device = result.getDevice();
+            connectDevice(device);
+         }
+      });
+
+      mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
       mScanner = mBluetoothAdapter.getBluetoothLeScanner();
    }
 
    void connectDevice(BluetoothDevice device) {
-      String name = device.getName();
-      String address = device.getAddress();
+      Log.d(TAG, "연결 시도 : " + device.getName());
 
       device.connectGatt(MainActivity.this, false, new BluetoothGattCallback() {
          @Override
@@ -124,22 +139,52 @@ public class MainActivity extends AppCompatActivity {
       });
    }
 
-   public void startDiscover(View v) {
-      deviceList.clear();
+   void toggleScan() {
+      if ( mScanning == true ) {
+         mScanner.stopScan(null);
+         mScanning = false;
+         mStatusView.setText("스캐닝 중지");
+      }
+      else {
+         startScan();
+         mScanning = true;
+         mStatusView.setText("스캐닝 시작");
 
+         // 10초뒤 자동 스캐닝 중지
+         mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               if ( mScanning ) {
+                  toggleScan();
+               }
+            }
+         }, 10 * 1000);
+      }
+   }
+
+   // LE 장치 검색
+   void startScan() {
+      deviceList.clear();
+      deviceAdapter.notifyDataSetChanged();
+
+      // from API 21
       mScanner.startScan(new ScanCallback() {
          @Override
          public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            Log.d(TAG, result.toString());
-
             deviceList.add(result);
             deviceAdapter.notifyDataSetChanged();
          }
 
          @Override
-         public void onScanFailed(int errorCode) {
+         public void onScanFailed(final int errorCode) {
             super.onScanFailed(errorCode);
+            mHandler.post(new Runnable() {
+               @Override
+               public void run() {
+                  mStatusView.setText("스캐닝 에러 : " + errorCode);
+               }
+            });
          }
 
          @Override
@@ -185,3 +230,4 @@ public class MainActivity extends AppCompatActivity {
       }
    }
 }
+
