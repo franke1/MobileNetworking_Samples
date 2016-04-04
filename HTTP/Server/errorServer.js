2e59fd4e997c565441d4839a92b69acfcc0bf8bb
wannabewize <wannabewize@gmail.com>
2016-04-05 00:49:22 +0900

    에러 처리 예제 서버 작성 중

--- HTTP/Server/errorServer.js
+++ HTTP/Server/errorServer.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var logger = require('morgan');
+var timeout = require('connect-timeout');
 var app = express();
+
+app.use(timeout('5s'));
 app.use(logger('dev'));
 
 app.listen(3000, function(err) {
    console.log('Server is Running 3000');
 });
-
-var num = 0;
-var resFuns = [successResponse, badrequestResponse, unauthorizedReseponse, infiniteResponse ];
 
 function successResponse(req, res) {
    res.sendStatus(200);
@@ -19,10 +19,11 @@ function infiniteResponse(req, res) {
 }
 
 
+// 10초뒤 응답
 function delayedResponse(req, res) {
    setTimeout(function() {
       res.status(200).send("Delayed Response");
-   }, 40 * 1000);
+   }, 10 * 1000);
 }
 
 function badrequestResponse(req, res) {
@@ -32,15 +33,9 @@ function badrequestResponse(req, res) {
 function unauthorizedReseponse(req, res) {
    res.status(401).send("Unauthorized");
 }
+
 //http://192.168.25.3:3000/infinite
 app.get('/infinite', infiniteResponse);
 app.get('/badreq', badrequestResponse);
 app.get('/unauth', unauthorizedReseponse);
 app.get('/delayed', delayedResponse);
-
-// app.use(function(req, res) {
-//    var index = num++ % resFuns.length;
-//    var handler = resFuns[index];
-//    console.log('Response Handler : ' + handler.name);
-//    handler(req, res);   
-// });
