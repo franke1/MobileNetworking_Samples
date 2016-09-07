4621288386be1cffd1d3a9c36c1f1938a0066ad2
wannabewize <wannabewize@gmail.com>
2016-09-08 01:49:51 +0900

    멀티캐스트 예제 업데이트
    쓰레드 구조 개선

--- Socket/UDP_Multicast_Android/app/src/main/java/com/example/wannabewize/udpmulticast_android/MainActivity.java
+++ Socket/UDP_Multicast_Android/app/src/main/java/com/example/wannabewize/udpmulticast_android/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -17,75 +18,62 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "UdpMulticast";
     private static final String MULTICAST_ADDRESS = "239.255.255.255";
     private static final int MULTICAST_PORT = 49001;
-    private MulticastSocket mSocket;
-    private MessageReceiveThread mReceiveThread;
+
+    private MessageThread messageThread;
     private Handler mHandler;
     private TextView mMessageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMessageView = (TextView) findViewById(R.id.messageView);
+        mHandler = new Handler();
+
         final EditText userInput = (EditText) findViewById(R.id.userInput);
+
         findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message = userInput.getText().toString();
-                sendMessage(message);
+                messageThread.sendMessage(message);
 
             }
         });
 
-        mHandler = new Handler();
-        mMessageView = (TextView) findViewById(R.id.messageView);
+        messageThread = new MessageThread();
+        messageThread.start();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mMessageView.setText("Multicast Example.\nMessage receiving from " + MULTICAST_ADDRESS);
-        readyUdpSocket();
-    }
+    // 메세지 수신/발신 용 쓰레드
+    class MessageThread extends Thread {
+        private MulticastSocket socket;
 
-    void readyUdpSocket() {
-        try {
-            mSocket = new MulticastSocket(MULTICAST_PORT);
-            mSocket.joinGroup(InetAddress.getByName(MULTICAST_ADDRESS));
-
-            mReceiveThread = new MessageReceiveThread(mSocket);
-            mReceiveThread.start();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e(TAG, "Socket Exception : " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    void sendMessage(final String message) {
-        new Thread() {
-            @Override
-            public void run() {
-                byte[] data = message.getBytes();
-                DatagramPacket packet = null;
-                try {
-                    packet = new DatagramPacket(data, data.length, InetAddress.getByName(MULTICAST_ADDRESS), MULTICAST_PORT);
-                    mSocket.send(packet);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        public MessageThread() {
+            try {
+                socket = new MulticastSocket(MULTICAST_PORT);
+                socket.joinGroup(InetAddress.getByName(MULTICAST_ADDRESS));
+            } catch (IOException e) {
+                Log.e(TAG, "Error " + e.getMessage());
+                e.printStackTrace();
             }
-        }.start();
-    }
+        }
 
-    // 메세지 수신 용 쓰레드
-    class MessageReceiveThread extends Thread {
-        private final MulticastSocket socket;
-
-        public MessageReceiveThread(MulticastSocket mSocket) {
-            this.socket = mSocket;
+        void sendMessage(final String message) {
+            new Thread() {
+                @Override
+                public void run() {
+                    byte[] data = message.getBytes();
+                    DatagramPacket packet = null;
+                    try {
+                        packet = new DatagramPacket(data, data.length, InetAddress.getByName(MULTICAST_ADDRESS), MULTICAST_PORT);
+                        socket.send(packet);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
         }
 
         @Override
@@ -95,8 +83,7 @@ public class MainActivity extends AppCompatActivity {
                     // 버퍼
                     byte[] buffer = new byte[65507];
                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                    // 블록 코드다.
-                    socket.receive(packet);
+                    socket.receive(packet); // Block-API
 
                     // 데이터그램 패킷에서
                     final String received = new String(packet.getData()).trim();
